0908275c78fe3de84722559402ce862085069460
Justin Fimbo <fimbo.isso@gmail.com>
2024-03-25 10:20:05 +0200

    implement footer and navbar as compoments

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return <footer className="main-footer">
+        <strong>Designed by &copy; 2024 <a href="https://afri-soft.com">Afri-Soft</a>.</strong>
+        All rights reserved.
+        <div className="float-right d-none d-sm-inline-block">
+            <b>Version</b> 1.0
+        </div>
+    </footer>;
+}
+
+export default Footer;
--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,8 @@ import "../assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css";
 // ! import componets
 import SidebarSearch from './Home/SidebarSearch';
 import UserProfilePanel from './Home/UserProfilePanel';
+import Footer from './Footer';
+import NavBar from './NavBar';
 // ! import js. no necessary to use jquery
 //import '../assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js';
 // import '../assets/dist/js/admin-lte/dist/js/adminlte';
@@ -50,111 +52,70 @@ const Home = () => {
   // }, []);
 
   return (
-          <div className="wrapper hold-transition light-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
-    
-            {/* Navbar */}
-            <nav className="main-header navbar navbar-expand navbar-light">
-              {/* Left navbar links */}
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link" data-widget="pushmenu" href="#" role="button"><i className="fas fa-bars"></i></a>
-                </li>
-                <li className="nav-item d-none d-sm-inline-block">
-                  <a href="index3.html" className="nav-link">Acceuil</a>
-                </li>
-              </ul>
-    
-              {/* Right navbar links */}
-              <ul className="navbar-nav ml-auto">
-                {/* Navbar Search */}
-                <li className="nav-item">
-                  <a className="nav-link" data-widget="navbar-search" href="#" role="button">
-                    <i className="fas fa-search"></i>
-                  </a>
-                  <div className="navbar-search-block">
-                    <form className="form-inline">
-                      <div className="input-group input-group-sm">
-                        <input className="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search" />
-                        <div className="input-group-append">
-                          <button className="btn btn-navbar" type="submit">
-                            <i className="fas fa-search"></i>
-                          </button>
-                          <button className="btn btn-navbar" type="button" data-widget="navbar-search">
-                            <i className="fas fa-times"></i>
-                          </button>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
-                </li>
-              </ul>
-            </nav>
-            {/* /.navbar */}
-    
-            {/* Main Sidebar Container */}
-            <aside className="main-sidebar sidebar-light-primary elevation-4">
-              {/* Brand Logo */}
-              <a href="index3.html" className="brand-link">
-                <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{ opacity: '.8' }} />
-                <span className="brand-text font-weight-light">K-FIDELITY</span>
-              </a>
-    
-              {/* Sidebar */}
-              <div className="sidebar">
-                {/* Sidebar content */}
-                <UserProfilePanel />
-                <SidebarSearch />
-                {/* Add sidebar content */}
+    <div className="wrapper hold-transition light-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+
+      {/* Navbar */}
+      <NavBar />
+      {/* /.navbar */}
+
+      {/* Main Sidebar Container */}
+      <aside className="main-sidebar sidebar-light-primary elevation-4">
+        {/* Brand Logo */}
+        <a href="index3.html" className="brand-link">
+          <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{ opacity: '.8' }} />
+          <span className="brand-text font-weight-light">K-FIDELITY</span>
+        </a>
+
+        {/* Sidebar */}
+        <div className="sidebar">
+          {/* Sidebar content */}
+          <UserProfilePanel />
+          <SidebarSearch />
+          {/* Add sidebar content */}
+        </div>
+        {/* /.sidebar */}
+      </aside>
+
+      {/* Content Wrapper. Contains page content */}
+      <div className="content-wrapper">
+        {/* Content Header (Page header) */}
+        <div className="content-header">
+          <div className="container-fluid">
+            <div className="row mb-2">
+              <div className="col-sm-6">
+                <h1 className="m-0">Acceuil </h1>
               </div>
-              {/* /.sidebar */}
-            </aside>
-    
-            {/* Content Wrapper. Contains page content */}
-            <div className="content-wrapper">
-              {/* Content Header (Page header) */}
-              <div className="content-header">
-                <div className="container-fluid">
-                  <div className="row mb-2">
-                    <div className="col-sm-6">
-                      <h1 className="m-0">Acceuil </h1>
-                    </div>
-                    <div className="col-sm-6">
-                      <ol className="breadcrumb float-sm-right">
-                        <li className="breadcrumb-item"><a href="#">Home</a></li>
-                        <li className="breadcrumb-item active">Dashboard</li>
-                      </ol>
-                    </div>
-                  </div>
-                </div>
+              <div className="col-sm-6">
+                <ol className="breadcrumb float-sm-right">
+                  <li className="breadcrumb-item"><a href="#">Home</a></li>
+                  <li className="breadcrumb-item active">Dashboard</li>
+                </ol>
               </div>
-              {/* /.content-header */}
-    
-              {/* Main content */}
-              <section className="content">
-                <div className="container-fluid">
-                  {/* Place your content here */}
-                </div>
-              </section>
-              {/* /.content */}
             </div>
-            {/* /.content-wrapper */}
-    
-            {/* Control Sidebar */}
-            <aside className="control-sidebar control-sidebar-dark">
-              {/* Control sidebar content */}
-            </aside>
-            {/* /.control-sidebar */}
-    
-            {/* Main Footer */}
-            <footer className="main-footer">
-              <strong>Designed by &copy; 2024 <a href="https://afri-soft.com">Afri-Soft</a>.</strong>
-              All rights reserved.
-              <div className="float-right d-none d-sm-inline-block">
-                <b>Version</b> 1.0
-              </div>
-            </footer>
           </div>
-      );
-    };
+        </div>
+        {/* /.content-header */}
+
+        {/* Main content */}
+        <section className="content">
+          <div className="container-fluid">
+            {/* Place your content here */}
+          </div>
+        </section>
+        {/* /.content */}
+      </div>
+      {/* /.content-wrapper */}
+
+      {/* Control Sidebar */}
+      <aside className="control-sidebar control-sidebar-dark">
+        {/* Control sidebar content */}
+      </aside>
+      {/* /.control-sidebar */}
+
+      {/* Main Footer */}
+      <Footer />
+    </div>
+  );
+};
 
 export default Home
